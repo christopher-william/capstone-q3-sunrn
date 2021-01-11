aa4cdfd4b87c468c9296d2ece612418b12f359f5
Cauan França <cauan.f.ramos@gmail.com>
2021-01-11 14:19:54 -0300

    feat: base structure done

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from .models import db
+from .models import mg
+from environs import Env
+
+from .views import configure as views_configure
+
+
+def create_app(mode='development'):
+    env = Env()
+    env.read_env()
+
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = env.bool(
+        'SQLALCHEMY_TRACK_MODIFICATIONS'
+    )
+    app.config['SQLALCHEMY_DATABASE_URI'] = env.str('SQLALCHEMY_DATABASE_URI')
+
+    db.init_app(app)
+    mg.init_app(app, db)
+
+    views_configure(app)
+
+    return app
---
+++ app/views/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+
+def configure(app: Flask):
+    # EXAMPLE
+    # from .users import bp_user
+    # app.register_blueprint(bp_user)
+    pass
