c3312350f307aad564fc09677ff4c04c3d2bdeab
Bruno Alexandre Gomes de Souza <souza.bruno.alexandre@gmail.com>
2021-01-11 22:25:51 -0300

    csv reader generator ready

--- app/services/hsp/main.py
+++ app/services/hsp/main.py
@@ -1,5 +1,24 @@
 import csv
 
+
 def hsp_readers(csv_string_name):
-  with open(csv_string_name,'r'):
-    pass
+    with open(csv_string_name, 'r') as file:
+
+        reader = csv.DictReader(
+            file
+        )
+        for hsp in reader:
+            new_hsp = {}
+            new_hsp.update({
+                "city": hsp["NAME"],
+                "uf": hsp["STATE"],
+                "md_anual": hsp["ANNUAL"],
+                "lon": hsp["LON"],
+                "lat": hsp["LAT"]
+            })
+
+            yield new_hsp
+
+
+for data in hsp_readers("hsp_data_2017.csv"):
+    print(data)
