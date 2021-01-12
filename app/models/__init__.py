2396cbe6b4cfa59408a71ad22b888f6567a9b1df
Edson Silva <edsonoguera@outlook.com>
2021-01-11 21:44:07 -0300

    feat: created migrate panel_price

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -29,3 +29,14 @@ class Inverter_price(db.Model):
 
     def __repr__(self):
         return f'<User {self.model}>'
+
+
+class Panel_price(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(10), nullable=False)
+    brand = db.Column(db.String(100), nullable=False)
+    power = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Numeric, nullable=False)
+
+    def __repr__(self):
+        return f"<Table name {self.Panel_price.__name__}>"
