8653d68ee25eb2759b5be4ca9fc23e3737e580f5
Edson Silva <edsonoguera@outlook.com>
2021-01-12 20:35:25 -0300

    vai

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -10,11 +10,33 @@ mg = Migrate()
 
 class Hsp(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    city = db.Column(db.String(80), unique=False, nullable=False)
-    uf = db.Column(db.String(120), unique=False, nullable=False)
+    city = db.Column(db.String(80), nullable=False)
+    uf = db.Column(db.String(120), nullable=False)
     md_anual = db.Column(db.Integer, nullable=False)
     lon = db.Column(db.Numeric, nullable=False)
     lat = db.Column(db.Numeric, nullable=False)
 
     def __repr__(self):
         return f'<User {self.city}>'
+
+
+class Inverter_price(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(10), nullable=False)
+    brand = db.Column(db.String(100), nullable=False)
+    power = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Numeric, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.model}>'
+
+
+class Panel_price(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(10), nullable=False)
+    brand = db.Column(db.String(100), nullable=False)
+    power = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Numeric, nullable=False)
+
+    def __repr__(self):
+        return f"<Table name {self.Panel_price.__name__}>"
