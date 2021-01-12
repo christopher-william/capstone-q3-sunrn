7d84f96cace9155de263acaa1b5c5bf49db1050f
Edson Silva <edsonoguera@outlook.com>
2021-01-11 21:31:08 -0300

    feat: included migrate inverter price

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -18,3 +18,14 @@ class Hsp(db.Model):
 
     def __repr__(self):
         return f'<User {self.city}>'
+
+
+class Inverter_price(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(10), nullable=False)
+    brand = db.Column(db.String(100), nullable=False)
+    power = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Numeric, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.model}>'
