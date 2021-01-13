02e18e8ff7e0893d4731cb48106c385a95f647a8
Bruno Alexandre Gomes de Souza <souza.bruno.alexandre@gmail.com>
2021-01-13 10:50:21 -0300

    feat: mockup db

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -15,6 +15,7 @@ class Hsp(db.Model):
     md_anual = db.Column(db.Integer, nullable=False)
     lon = db.Column(db.Numeric, nullable=False)
     lat = db.Column(db.Numeric, nullable=False)
+    leads = db.relationship('Lead', backref='hsp', lazy=True)
 
     def __repr__(self):
         return f'<User {self.city}>'
@@ -40,3 +41,23 @@ class Panel_price(db.Model):
 
     def __repr__(self):
         return f"<Table name {self.Panel_price.__name__}>"
+
+
+class Energy_data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    leads = db.relationship('Lead', backref='energy_data', lazy=True)
+
+
+class Lead(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String, nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    phone = db.Column(db.String(20), nullable=False, unique=True)
+    hsp_id = db.Column(db.Integer, db.ForeignKey('hsp.id'))
+    energy_id = db.Column(db.Integer, db.ForeignKey('energy_data.id'))
+    simulations = db.relationship('Simulation', backref='lead', lazy=True)
+
+
+class Simulation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lead_id = db.Column(db.Integer, db.ForeignKey('lead.id'))
