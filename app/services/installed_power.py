ecd9a96782794a67d8bf201e238ae824f46dacd0
Bruno Alexandre Gomes de Souza <souza.bruno.alexandre@gmail.com>
2021-01-13 13:01:36 -0300

    docs:eficience change

--- app/services/installed_power.py
+++ app/services/installed_power.py
@@ -6,7 +6,7 @@ def installed_power(db_hsp: Hsp, energy_data: Energy_data):
     hsp = db_hsp.md_anual/1000
     diary_energy = energy_data.month_energy/30
     ppico = diary_energy/hsp
-    eficience = 1 - 0.2
+    eficience = 0.8
     ppico_real = ppico/eficience
 
     return ppico_real
