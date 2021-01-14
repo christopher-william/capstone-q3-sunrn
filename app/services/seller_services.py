c3947ef1acc1a4def1d6676a658b6939219d9428
felipe <felipematheusmellodemorais@gmail.com>
2021-01-14 09:50:30 -0300

    feature: encoded password implemented

--- app/services/seller_services.py
+++ app/services/seller_services.py
@@ -5,12 +5,14 @@ from app.models import db
 from app.models.seller_model import Seller, seller_schema
 from sqlalchemy.exc import IntegrityError
 from .http import build_api_response
-
+from app.services.encode_service import encode_password
 
 def login_seller(data):
     try:
+        encoded_password = encode_password(data)
+
         seller = Seller.query.filter_by(
-            email=data['email'], password=data['password']).first()
+            email=data['email'], password=encode_password).first()
 
         if not seller:
             return build_api_response(HTTPStatus.NOT_FOUND)
@@ -22,11 +24,11 @@ def login_seller(data):
 
 
 def create_seller(data):
-
+    encoded_password = encode_password(data)
     seller = Seller(
         name=data['name'],
         email=data['email'],
-        password=data['password']
+        password=encoded_password
     )
 
     try:
