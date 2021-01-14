52b6e2e9a41ef27517eb9883177488dc488e5936
Bruno Alexandre Gomes de Souza <souza.bruno.alexandre@gmail.com>
2021-01-14 12:33:52 -0300

    feat: inversor selector ready

--- app/services/calculate_roi_panel_service.py
+++ app/services/calculate_roi_panel_service.py
@@ -6,3 +6,13 @@ def panel_costs(pow_pico, pow_panel, price_panel):
     panel_set_price = ((pow_pico//pow_panel) + 1) * price_panel
 
     return panel_set_price
+
+
+def inversor_selector(inversor_list):
+    def sort_inversors(e):
+        return e.price
+
+    available_inversors = list(
+        filter(lambda inversor: inversor.power/0.8 >= ppico, inversor_list))
+    available_inversors.sort(key=sort_inversors)
+    return available_inversors[0]
