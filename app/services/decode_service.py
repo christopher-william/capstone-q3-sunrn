3f2d752a1e59f6dd55c92a98b0b6fcd3787e30b3
felipe <felipematheusmellodemorais@gmail.com>
2021-01-16 16:28:07 -0300

    feature: test decode service done

--- app/services/decode_service.py
+++ app/services/decode_service.py
@@ -1,7 +1,7 @@
-from jwt import decode
+import jwt
 
 def decode_password(encoded_data, key):
     decoded_data = jwt.decode(encoded_data, key
     , algorithms=["HS256"])
 
-    return decoded_data
+    return decoded_data.get('email')
--- tests/services/test_decode_service.py
+++ tests/services/test_decode_service.py
@@ -1,2 +1,16 @@
-def should_return_a_decoded_data():
+from app.services.decode_service import decode_password
+from app.services.encode_service import encode_password
+
+def test_should_return_a_decoded_data():
+    data = {
+        'name': 'Carlitos',
+        'email': 'carlitos@gmail.com',
+        'password': '1234'
+    }
+    encoded_data = encode_password(data)
+    key = data['password']
     
+    result = decode_password(encoded_data, key)
+    expected = data['email']
+
+    assert result == expected
