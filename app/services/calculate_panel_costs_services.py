d8bc930725061f1f1a3d441d841f8a09db341bd4
Bruno Alexandre Gomes de Souza <souza.bruno.alexandre@gmail.com>
2021-01-18 11:19:36 -0300

    feat: panel selector ready

--- app/services/calculate_panel_costs_services.py
+++ app/services/calculate_panel_costs_services.py
@@ -1,6 +1,3 @@
-from app.models import Hsp, Energy_data, Panel_price, energy_data_model
-
-
 def panel_costs(pow_pico, pow_panel, price_panel):
     pow_pico = pow_pico * 1000
     panel_set_price = ((pow_pico//pow_panel) + 1) * price_panel
--- app/services/calculate_roi_panel_service.py
+++ app/services/calculate_roi_panel_service.py
@@ -1,7 +1,7 @@
 from app.models import Hsp, Energy_data, Panel_price, energy_data_model
 
 
-def panel_costs(pow_pico, pow_panel, price_panel):
+def panel_set_costs(pow_pico, pow_panel, price_panel):
     pow_pico = pow_pico * 1000
     panel_set_price = ((pow_pico//pow_panel) + 1) * price_panel
 
@@ -16,3 +16,18 @@ def inversor_selector(inversor_list):
         filter(lambda inversor: inversor.power/0.8 >= ppico, inversor_list))
     available_inversors.sort(key=sort_inversors)
     return available_inversors[0]
+
+
+def panel_selector(panel_list, pow_pico):
+
+    def sort_panel(e):
+        return panel_set_costs(pow_pico, e.power, e.price)
+
+    panel_list.sort(key=sort_panel)
+    selected_panel = panel_list[0]
+    selected_panel_quantity = pow_pico//selected_panel.power + 1
+
+    return {
+        "panel": selected_panel,
+        "quantity": selected_panel_quantity
+    }
