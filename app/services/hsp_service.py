661003cf5ebdc1b9270bee828ef741f042b6981d
Cauan França <cauan.f.ramos@gmail.com>
2021-01-21 12:56:08 -0300

    fix: route hsp fixed

--- app/services/hsp_service.py
+++ app/services/hsp_service.py
@@ -3,12 +3,16 @@ from http import HTTPStatus
 from app.models.hsp_model import Hsp, hsps_schema
 from sqlalchemy.exc import IntegrityError
 
-from .http_service import build_api_response
-
+from .http_service import build_api_response, build_response_message
+from .uf_dict_service import ufs_dict
 
 def get_uf_or_all(**kwargs):
+    request_uf = kwargs.get("uf")
+    if request_uf and request_uf.isupper():
+        return "Not found", HTTPStatus.NOT_FOUND
+    
     try:
-        uf = kwargs["uf"].upper()
+        uf = ufs_dict[request_uf]
         ufs = Hsp.query.filter_by(uf=uf).all()
         if not len(ufs):
             return build_api_response(HTTPStatus.BAD_REQUEST)
@@ -17,9 +21,6 @@ def get_uf_or_all(**kwargs):
             "data": hsps_schema.dump(ufs)})
 
     except:
-        uf_query = Hsp.query.order_by(Hsp.uf).all()
-        uf_schema = hsps_schema.dump(uf_query)
-        uf_list = [uf.get("uf") for uf in uf_schema]
-        uf_filtred = list(set(uf_list))
+        uf_list = list(ufs_dict.keys())
 
-        return build_api_response(HTTPStatus.OK, {"uf": uf_filtred})
+        return build_api_response(HTTPStatus.OK, {"uf": uf_list })
